78701132f041bf0be9aad8213f24f4d77e6d7b36
Denis Arsenault <darsenault@me.com>
2018-09-12 10:19:27 -0400

    Finalized Changes

---
+++ sndrContact/sndrEncrypt.h
@@ -0,0 +1,13 @@
+//
+//  sndrEncrypt.h
+//  sndrContact
+//
+//  Created by Denis Arsenault on 9/11/18.
+//  Copyright © 2018 sndr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sndrEncrypt : NSObject
+
+@end
---
+++ sndrContact/sndrEncrypt.m
@@ -0,0 +1,13 @@
+//
+//  sndrEncrypt.m
+//  sndrContact
+//
+//  Created by Denis Arsenault on 9/11/18.
+//  Copyright © 2018 sndr. All rights reserved.
+//
+
+#import "sndrEncrypt.h"
+
+@implementation sndrEncrypt
+
+@end
